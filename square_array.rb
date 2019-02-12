b562e2cd0fc786a99da61552ca7469f59313f7a6
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-12 23:44:37 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,11 @@
 def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
+arr = []
+array.each { |i| arr << i ** 2 }
+arr
 end
+
+#def square_array(array)
+#  new_array = []
+#  new_array = array.collect {|i| array << i**2 }
+#  new_array
+#end
